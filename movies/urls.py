2e5701dc2ba77ca6679a26c531e883f4a4ee999a
Filip Barański <filip.baaranski@gmail.com>
2024-06-19 13:49:23 +0200

    Cast, Genres, Person Views, Urls, pytest.ini

--- movies/urls.py
+++ movies/urls.py
@@ -9,4 +9,19 @@ urlpatterns = [
     path('movies/add/', MovieCreateView.as_view(), name='movie_add'),
     path('movies/<int:pk>/edit/', MovieUpdateView.as_view(), name='movie_edit'),
     path('movies/<int:pk>/delete/', MovieDeleteView.as_view(), name='movie_delete'),
+
+    path('casts/add/', CastCreateView.as_view(), name='cast_add'),
+    path('casts/<int:pk>/edit/', CastUpdateView.as_view(), name='cast_edit'),
+    path('casts/<int:pk>/delete/', CastDeleteView.as_view(), name='cast_delete'),
+
+    path('genres/', GenreListView.as_view(), name='genre_list'),
+    path('genres/add/', GenreCreateView.as_view(), name='genre_add'),
+    path('genres/<int:pk>/edit/', GenreUpdateView.as_view(), name='genre_edit'),
+    path('genres/<int:pk>/delete/', GenreDeleteView.as_view(), name='genre_delete'),
+
+    path('persons/', PersonListView.as_view(), name='person_list'),
+    path('persons/add/', PersonCreateView.as_view(), name='person_add'),
+    path('persons/<int:pk>/edit/', PersonUpdateView.as_view(), name='person_edit'),
+    path('persons/<int:pk>/delete/', PersonDeleteView.as_view(), name='person_delete'),
+    path('persons/<int:pk>/', PersonDetailView.as_view(), name='person_detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,10 @@
 import logging
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-from movies.forms import MovieForm
-from movies.models import Movie, Review
+from movies.forms import MovieForm, PersonForm, GenreForm, CastForm
+from movies.models import Movie, Review, Person, Genre, Cast
 
 
 class HomeView(TemplateView):
@@ -61,3 +61,123 @@ class MovieDeleteView(LoginRequiredMixin, DeleteView):
 
 
 logger = logging.getLogger(__name__)
+
+
+class CastCreateView(LoginRequiredMixin, CreateView):
+    model = Cast
+    form_class = CastForm
+    template_name = 'movies/cast_form.html'
+
+    def form_valid(self, form):
+        movie_id = self.request.GET.get('movie')
+        logger.debug(f"Creating cast for movie ID: {movie_id}")
+        form.instance.movie = get_object_or_404(Movie, pk=movie_id)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        movie_pk = self.object.movie.pk
+        logger.debug(f"Redirecting to movie detail for movie ID: {movie_pk}")
+        return reverse_lazy('movie_detail', kwargs={'pk': movie_pk})
+
+
+class CastUpdateView(LoginRequiredMixin, UpdateView):
+    model = Cast
+    form_class = CastForm
+    template_name = 'movies/cast_form.html'
+
+    def get_success_url(self):
+        movie_pk = self.object.movie.pk
+        logger.debug(f"Redirecting to movie detail for movie ID: {movie_pk}")
+        return reverse_lazy('movie_detail', kwargs={'pk': movie_pk})
+
+
+class CastDeleteView(LoginRequiredMixin, DeleteView):
+    model = Cast
+    template_name = 'movies/cast_confirm_delete.html'
+
+    def get_success_url(self):
+        movie_pk = self.get_object().movie.pk
+        logger.debug(f"Redirecting to movie detail for movie ID: {movie_pk}")
+        return reverse_lazy('movie_detail', kwargs={'pk': movie_pk})
+
+
+class GenreListView(ListView):
+    model = Genre
+    template_name = 'movies/genre_list.html'
+    context_object_name = 'genres'
+
+
+class GenreCreateView(LoginRequiredMixin, CreateView):
+    model = Genre
+    form_class = GenreForm
+    template_name = 'movies/genre_form.html'
+    success_url = reverse_lazy('genre_list')
+
+
+class GenreUpdateView(LoginRequiredMixin, UpdateView):
+    model = Genre
+    form_class = GenreForm
+    template_name = 'movies/genre_form.html'
+    success_url = reverse_lazy('genre_list')
+
+
+class GenreDeleteView(LoginRequiredMixin, DeleteView):
+    model = Genre
+    template_name = 'movies/genre_confirm_delete.html'
+    success_url = reverse_lazy('genre_list')
+
+
+class PersonListView(ListView):
+    model = Person
+    template_name = 'movies/person_list.html'
+    context_object_name = 'persons'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        role = self.request.GET.get('role')
+        queryset = Person.objects.all()
+        if role:
+            if role == 'actor':
+                return Person.objects.filter(role__in=['actor', 'both'])
+            elif role == 'director':
+                return Person.objects.filter(role__in=['director', 'both'])
+        if query:
+            queryset = queryset.filter(first_name__icontains=query) | queryset.filter(last_name__icontains=query)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        for person in context['persons']:
+            if person.role == 'both':
+                person.display_role = 'Actor, Director'
+            elif person.role == 'actor':
+                person.display_role = 'Actor'
+            elif person.role == 'director':
+                person.display_role = 'Director'
+        return context
+
+
+class PersonCreateView(LoginRequiredMixin, CreateView):
+    model = Person
+    form_class = PersonForm
+    template_name = 'movies/person_form.html'
+    success_url = reverse_lazy('person_list')
+
+
+class PersonUpdateView(LoginRequiredMixin, UpdateView):
+    model = Person
+    form_class = PersonForm
+    template_name = 'movies/person_form.html'
+    success_url = reverse_lazy('person_list')
+
+
+class PersonDeleteView(LoginRequiredMixin, DeleteView):
+    model = Person
+    template_name = 'movies/person_confirm_delete.html'
+    success_url = reverse_lazy('person_list')
+
+
+class PersonDetailView(DetailView):
+    model = Person
+    template_name = 'movies/person_detail.html'
+    context_object_name = 'person'
